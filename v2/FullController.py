3bf7e316d465421bb90b069a815374e7dd0cd0a1
MandicFilip <filipmandic26@gmail.com>
2022-10-16 16:19:58 +0200

    added stats, not tested

--- v2/FullController.py
+++ v2/FullController.py
@@ -29,6 +29,8 @@ from ryu.lib.packet import udp
 from ryu import cfg
 from ryu.lib import hub
 
+import statistics
+import scipy.stats
 import shlex
 import subprocess
 from operator import itemgetter
@@ -313,11 +315,15 @@ class MatchV2:
         return {'ip_src': self.ip_src, 'ip_dst': self.ip_dst,
                 'port_src': self.port_src, 'port_dst': self.port_dst, 'protocol_code': self.protocol_code}
 
+    def is_tcp(self):
+        return self.protocol_code == in_proto.IPPROTO_TCP
+
     def is_udp(self):
         return self.protocol_code == in_proto.IPPROTO_UDP
 
     def to_string(self):
-        return '{ip_src: ' + self.ip_src + ', ip_dst: ' + self.ip_dst + ', port_src: ' + str(self.port_src) + ', port_dst:' + str(self.port_dst) + '}'
+        return '{ip_src: ' + self.ip_src + ', ip_dst: ' + self.ip_dst + ', port_src: ' + str(self.port_src) + \
+               ', port_dst:' + str(self.port_dst) + '}'
 
 
 class DataV2:
@@ -335,6 +341,30 @@ class DataV2:
         self.tcp_flags_byte_count = 0
         self.tcp_flags_packet_count = 0
         self.tcp_flags_interval = 0
+
+        self.byte_mean = 0
+        self.packet_mean = 0
+
+        self.byte_median = 0
+        self.packet_median = 0
+
+        self.byte_mode = 0
+        self.packet_mode = 0
+
+        self.byte_standard_deviation = 0
+        self.packet_standard_deviation = 0
+
+        self.byte_fisher_skew = 0
+        self.packet_fisher_skew = 0
+
+        self.byte_fisher_kurtosis = 0
+        self.packet_fisher_kurtosis = 0
+
+        self.byte_correlation = 0
+        self.packet_correlation = 0
+
+    def get_lists(self):
+        return self.byte_count_list, self.packet_count_list
 
     # public - called from update
     def append_data(self, total_byte_count, total_packet_count):
@@ -391,6 +421,74 @@ class DataV2:
         packet_count = total_packet_count - self.previous_packet_count
         return byte_count, packet_count
 
+    def calc_self_stats(self):
+        if len(self.byte_count_list) < MIN_LENGTH:
+            self.calc_mean()
+            self.calc_median()
+            self.calc_mode()
+            self.calc_standard_deviation()
+            self.calc_skew()
+            self.calc_kurtosis()
+
+    def calc_mean(self):
+        self.byte_mean = statistics.mean(self.byte_count_list)
+        self.packet_mean = statistics.mean(self.packet_count_list)
+
+    def calc_median(self):
+        self.byte_median = statistics.median(self.byte_count_list)
+        self.packet_median = statistics.median(self.packet_count_list)
+
+    def calc_mode(self):
+        self.byte_mode = scipy.stats.mode(self.byte_count_list).mode[0]
+        self.packet_mode = scipy.stats.mode(self.packet_count_list).mode[0]
+
+    def calc_standard_deviation(self):
+        self.byte_standard_deviation = statistics.stdev(self.byte_count_list)
+        self.packet_standard_deviation = statistics.stdev(self.packet_count_list)
+
+    def calc_skew(self):
+        self.byte_fisher_skew = scipy.stats.skew(self.byte_count_list)
+        self.packet_fisher_skew = scipy.stats.skew(self.packet_count_list)
+
+    def calc_kurtosis(self):
+        self.byte_fisher_kurtosis = scipy.stats.kurtosis(self.byte_count_list)
+        self.packet_fisher_kurtosis = scipy.stats.kurtosis(self.packet_count_list)
+
+    def calc_correlation(self, pair_byte_list, pair_packet_list):
+        if len(self.byte_count_list) < MIN_LENGTH:
+            self.byte_correlation = scipy.stats.pearsonr(self.byte_count_list, pair_byte_list)
+            self.packet_correlation = scipy.stats.pearsonr(self.packet_count_list, pair_packet_list)
+
+    def push_front(self, count, val):
+        for x in range(count):
+            self.byte_count_list.insert(0, val)
+            self.packet_count_list.insert(0, val)
+
+    def push_back(self, count, val):
+        for x in range(count):
+            self.byte_count_list.append(val)
+            self.packet_count_list.append(val)
+
+    def pop_front(self, count):
+        for x in range(count):
+            self.byte_count_list.pop(0)
+            self.packet_count_list.pop(0)
+
+    def pop_back(self, count):
+        for x in range(count):
+            self.byte_count_list.pop()
+            self.packet_count_list.pop()
+
+    def delay_start(self):
+        if len(self.byte_count_list) == 1:
+            return
+
+        first_interval_bytes = self.byte_count_list.pop(0)
+        first_interval_packet = self.packet_count_list.pop(0)
+
+        self.byte_count_list[0] = self.byte_count_list[0] + first_interval_bytes
+        self.packet_count_list[0] = self.packet_count_list[0] + first_interval_packet
+
 
 class FlowInfoV2:
     def __init__(self, start_interval, data, udp_idle_interval):
@@ -441,6 +539,55 @@ class FlowInfoV2:
     def to_string_match(self):
         return self.match.to_string()
 
+    def align_tcp_interval_start(self, pair):
+        if self.match.is_tcp() and pair.match.is_tcp():
+            pair_start = pair.get_start_interval()
+            if self.start_interval == pair_start + 1:
+                self.delay_start_interval()
+                return
+
+            if self.start_interval == pair_start - 1:
+                pair.delay_start_interval()
+                return
+
+            if self.start_interval < pair_start:
+                diff = pair_start - self.start_interval
+                pair.align_permanently_flow_beginnings(diff)
+
+            if self.start_interval > pair_start:
+                diff = self.start_interval - pair_start
+                self.align_permanently_flow_beginnings(diff)
+
+    def get_start_interval(self):
+        return self.start_interval
+
+    def delay_start_interval(self):
+        self.start_interval = self.start_interval + 1
+        self.data.delay_start()
+
+    def get_data_lists(self):
+        return self.data.get_lists()
+
+    def get_data_lists_length(self):
+        byte_list, packet_list = self.data.get_lists()
+        return len(byte_list)
+
+    def align_permanently_flow_beginnings(self, count):
+        self.start_interval = self.start_interval - count
+        self.data.push_front(count, 0)
+
+    def add_front_alignment(self, count):
+        self.data.push_front(count, 0)
+
+    def remove_front_alignment(self, count):
+        self.data.pop_front(count)
+
+    def add_back_alignment(self, count):
+        self.data.push_back(count, 0)
+
+    def remove_back_alignment(self, count):
+        self.data.pop_back(count)
+
     # -----------------------------------------COUNTERS FUNCTIONS-------------------------------------------------------
 
     def update_counters(self, total_byte_count, total_packet_count):
@@ -463,6 +610,56 @@ class FlowInfoV2:
     def process_dns(self):
         self.data.append_data(0, 0)
         self.status = STATUS_FINISHED
+
+    def calc_stats(self):
+        self.data.calc_self_stats()
+        if self.pair is not None:
+            self_start_diff, self_end_diff, pair_start_diff, pair_end_diff = self.align_lists()
+            pair_byte_list, pair_packet_list = self.pair.get_data_lists()
+            self.data.calc_correlation(pair_byte_list, pair_packet_list)
+            self.restore_original_lists(self_start_diff, self_end_diff, pair_start_diff, pair_end_diff)
+
+    def align_lists(self):
+        self_start_diff = 0
+        self_end_diff = 0
+        pair_start_diff = 0
+        pair_end_diff = 0
+
+        pair_start = self.pair.get_start_interval()
+        if self.start_interval < pair_start:
+            pair_start_diff = pair_start - self.start_interval
+            self.pair.add_front_alignment(pair_start_diff)
+
+        if self.start_interval > pair_start:
+            self_start_diff = self.start_interval - pair_start
+            self.add_front_alignment(self_start_diff)
+
+        self_list_len = self.get_data_lists_length()
+        pair_list_len = self.pair.get_data_lists_length()
+
+        if self_list_len < pair_list_len:
+            pair_end_diff = pair_list_len - self_list_len
+            self.pair.add_back_alignment(pair_end_diff)
+
+        if self_list_len > pair_list_len:
+            self_end_diff = self_list_len - pair_list_len
+            self.add_back_alignment(self_end_diff)
+
+        return self_start_diff, self_end_diff, pair_start_diff, pair_end_diff
+
+    def restore_original_lists(self, self_start_diff, self_end_diff, pair_start_diff, pair_end_diff):
+        if self_start_diff > 0:
+            self.remove_front_alignment(self_start_diff)
+
+        if self_end_diff > 0:
+            self.remove_back_alignment(self_end_diff)
+
+        if pair_start_diff > 0:
+            self.pair.remove_front_alignment(pair_start_diff)
+
+        if pair_end_diff > 0:
+            self.pair.remove_back_alignment(pair_end_diff)
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------TABLE-----------------------------------------------------------
@@ -554,6 +751,7 @@ class FlowDataTableV2:
                         if not pair.has_pair():
                             flow.set_pair(pair)
                             pair.set_pair(flow)
+                            flow.align_tcp_interval_start()
             i = i + 1
 
         i = 0
@@ -578,12 +776,14 @@ class FlowDataTableV2:
         else:
             index = self.find_dns_flow(removed_flow_data)
             if index != -1:
-                self.dns_flows[index].set_finished()
+                self.finished_flows.append(self.dns_flows[index])
+                self.dns_flows.pop(index)
             else:
                 print('Can not find finished flow\n' + str(removed_flow_data))
 
     # public
     def on_tcp_flags_package(self, tcp_flow_data):
+        print('TCP flags processing')
         index = self.find_active_flow(tcp_flow_data)
         if index != -1:
             flow = self.active_flows[index]
@@ -601,10 +801,11 @@ class FlowDataTableV2:
 
     # public
     def on_save_flows(self, active_flows_file, finished_flows_file):
-        #self.calc_stats()
+        self.calc_stats()
         save_active_flows(active_flows_file, self.active_flows)
         save_finished_flows(finished_flows_file, self.finished_flows)
         save_finished_flows(finished_flows_file, self.dns_flows)
+        self.clear_finished_flows()
 
     def find_active_flow(self, key):
         return find_flow(self.active_flows, key)
@@ -626,10 +827,17 @@ class FlowDataTableV2:
             return self.finished_flows[index]
         return None
 
+    def calc_stats(self):
+        for flow in self.active_flows:
+            flow.calc_stats()
+
+        for flow in self.finished_flows:
+            flow.calc_stats()
 
 # ----------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------CONTROLLER--------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------
+
 
 def extract_data_from_pkt(pkt):
     eth = pkt.get_protocols(ethernet.ethernet)[0]
@@ -973,6 +1181,7 @@ class SwitchController(app_manager.RyuApp):
 
     def save_tcp_flags_packet_info(self, packet):
         data = extract_data_from_pkt(packet)
+        print('Controller -> TCP flags processing ')
 
         # ipv6 not supported at the moment
         if data is not None:
